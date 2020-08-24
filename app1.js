563d3a1468ad97871ad35d0a71f2f8f117e82338
hafeez8165526 <hafeez8165526@gmail.com>
2020-08-24 14:27:19 +0530

    last commit

--- app1.js
+++ app1.js
@@ -76,17 +76,17 @@ async function change() {
 					break;
 				}
 			}
-			stateUrl = 'http://battuta.medunes.net/api/region/' + cd + '/all/?key=8555467e4eaa9e78c4257e76f4dff90b';
+			stateUrl = 'https://battuta.medunes.net/api/region/' + cd + '/all/?key=8555467e4eaa9e78c4257e76f4dff90b';
 			getStates(stateUrl, targetstates);
 		}
 	};
-	xhttp.open('GET', 'http://battuta.medunes.net/api/country/all/?key=8555467e4eaa9e78c4257e76f4dff90b');
+	xhttp.open('GET', 'https://battuta.medunes.net/api/country/all/?key=8555467e4eaa9e78c4257e76f4dff90b');
 	xhttp.send();
 }
 
 function changeCities() {
 	cityUrl =
-		'http://battuta.medunes.net/api/city/' +
+		'https://battuta.medunes.net/api/city/' +
 		cd +
 		'/search/?region=' +
 		targetstates.value +
@@ -106,7 +106,7 @@ function changeCities() {
 	console.log(cityUrl);
 	getCities(cityUrl, targetCities);
 }
-getCountries('http://battuta.medunes.net/api/country/all/?key=8555467e4eaa9e78c4257e76f4dff90b', targetCountry);
+getCountries('https://battuta.medunes.net/api/country/all/?key=8555467e4eaa9e78c4257e76f4dff90b', targetCountry);
 //getCode(saveCountries, targetCountry.value);
 function fun() {
 	alert('Submission Succesfull!!');
